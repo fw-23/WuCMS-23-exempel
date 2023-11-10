438ab5833af4e3fa57f439842299dd772fc5a4d4
Fredrik Welander <welandfr@arcada.fi>
2023-11-10 11:44:47 +0200

    lektion 6

---
+++ lektion-6/script.js
@@ -0,0 +1,30 @@
+
+let a = 100
+
+if (a == 1) {
+  console.log("a är 1")
+} else {
+  console.log("a är INTE 1")
+}
+
+let b = 6
+
+if (b != 5) { // != inte lika med
+    console.log("b är INTE 5")
+}
+
+if (b > 5) { // större än
+    console.log("B är större än 5")
+} else {
+    console.log("B är mindre än eller lika med 5")
+}
+
+let weather = "regn";
+
+if (weather == "regn") {
+    console.log("Kom ihåg paraplyet!")
+} else if (weather == "sol") {
+    console.log("Kom ihåg solkrämen")
+} else {
+    console.log("Jag känner inte till det vädret")
+}
